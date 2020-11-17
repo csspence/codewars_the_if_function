75977a80dd013f996b7440c949b38e45eb67899a
Crew Spence <crewspence@gCrewSpence.local>
2020-11-17 04:52:08 -0600

    Wrote working solution.

--- codewars_the_if_function.js
+++ codewars_the_if_function.js
@@ -9,5 +9,5 @@ _if(true, function(){console.log("True")}, function(){console.log("false")})
 */
 
 const _if = (bool, func1, func2) => {
-  bool === true ? func1() : func2();
+  bool ? func1() : func2();
 }
